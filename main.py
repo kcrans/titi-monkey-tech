6a3520a750116613d69996736c8195aec2738900
Kaleb Crans <69497802+kcrans@users.noreply.github.com>
2022-04-28 09:02:34 -0700

    Created main.py file

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+main function for python app
+"""
+
+__author__ = "Kaleb Crans"
+__version__ = "0.0.1"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    print("app started")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
