4cf4892f02f6ed98dc36981953f9ab9b78e50bff
Kaleb Crans <69497802+kcrans@users.noreply.github.com>
2022-07-01 13:08:34 -0700

    Updated go signal.py

--- go_signal.py
+++ go_signal.py
@@ -11,11 +11,11 @@ positive_reinforcement_delay = 1.0
 hold_phase_delay = 0.5 
 session_timeout_time = 30 # Normallly 480.0 seconds
 
-mon = monitors.Monitor('kalebs_desktop')
+mon = monitors.Monitor('macbook')
 print(prefs.general['winType'])
 #[500, 500]
 #create a window
-mywin = visual.Window(size=[2048, 1152], fullscr=False, color="black", monitor=mon, units="height")
+mywin = visual.Window(size=[1440, 900], fullscr=False, color="black", monitor=mon, units="height")
 #mywin.monitor.setCurrent('macbook.json')
 
 #create a mouse event class to track touch input
@@ -26,7 +26,9 @@ click_sound = Sound('clickSound.wav', name='clicksound')
 neg_reinforce_sound = Sound('negativeReinforcement.wav', name='negsound')
 
 
-info = {'name':'charles', 'ExpVersion':1.1 }
+phases = {'Go Signal':0, 'Wait Screen':1, 'Alternating Stop Signal':2, 'Random Stop Signal':3, 'Experiment':4}
+
+info = {'Subject ID':'charles', 'Subject Condition':'good', 'Phase': 'Alternating Stop Signal' }
 info['dateStr'] = data.getDateStr()
 infoDlg = gui.DlgFromDict(dictionary=info, title= 'test', fixed=['ExpVersion'])
 if infoDlg.OK:
@@ -34,10 +36,12 @@ if infoDlg.OK:
 else:
     print('no')
 
-fileName = info['name'] + info['dateStr']
+fileName = info['Subject ID'] + info['dateStr']
 dataFile = open(fileName+'.csv', 'w')  # a simple text file with 'comma-separated-values'
-dataFile.write('name,time,success\n')
-
+dataFile.write("subject id,subject condition,session timestamp,phase,trial number,stop stimulus,screen touched,response time,hold phase touches,direct touch,diameter\n")
+def write_data(trial_num, stop_stim, screen_touched, response_time, direct_touch):
+    dataFile.write(f"{info['Subject ID']},{info['Subject Condition']},{info['dateStr']},{phases[info['Phase']]},{trial_num},{stop_stim},{screen_touched},{response_time},0,{direct_touch},713\n")
+    return
 
 #create circle stimuli
 circle = visual.ShapeStim(
@@ -59,52 +63,72 @@ print(circle.units)
 #draw the stimuli and update the window
 
 globalClock = core.Clock()
+starttime = globalClock.getTime()
 trialClock = core.Clock()
 shapes = [circle, triangle] # 
 
 i = 0
 
-added = np.array([[0.4, 0] for i in range(100)])
-right = circle.vertices + added
-left = circle.vertices - added
+if info['Phase'] == 'Wait Screen':
+    def new_shape(x):
+        return 0
+elif info['Phase'] == 'Alternating Stop Signal':
+    def new_shape(x):
+        return x % 2
+        
+elif info['Phase'] == 'Random Stop Signal':
+    def new_shape(x): 
+        return choice((0, 1))
 
-print(np.shape(added))
+
 while globalClock.getTime() < session_timeout_time - 6.0:
-    index = i % 2
+    index = new_shape(i)
     dis_shape = shapes[index]
     mywin.flip()
     side = choice((-1, 1))
     dis_shape.pos = (side*(0.4 - index*0.1), index*-.125)
-    #if index == 0:
-     #   if side == -1:
-     #       dis_shape.vertices = left
-     #   else:
-      #      dis_shape.vertices = right
     trial_start_sound.play()
     core.wait(hold_phase_delay)
     trialClock.reset()
-    while trialClock.getTime() < 30.0 and globalClock.getTime() < session_timeout_time:
-        event.clearEvents()
-        dis_shape.draw()
-        mywin.update()
-        touch_tracker.clickReset()
+    if index == 0:
+        touched = False
+        while trialClock.getTime() < 30.0 and globalClock.getTime() < session_timeout_time:
+            event.clearEvents()
+            dis_shape.draw()
+            mywin.update()
+            touch_tracker.clickReset() #Maybe run once outside the loop?
 
-        if touch_tracker.getPressed(getTime=True)[0][0] == 1:
-            if dis_shape.contains(touch_tracker) and index == 0:
-                dataFile.write(f'{info["name"]},{trialClock.getTime()},type,yes\n')
+            if touch_tracker.getPressed(getTime=True)[0][0] == 1:
+                touched = True
+                if dis_shape.contains(touch_tracker) and index == 0:
+                    write_data(i + 1, 'FALSE', 'TRUE', trialClock.getTime(), 'TRUE')
+                    click_sound.play()
+                    mywin.flip()
+                    core.wait(positive_reinforcement_delay)
+                else:
+                    write_data(i + 1, 'FALSE', 'TRUE', trialClock.getTime(), 'FALSE')
+                    neg_reinforce_sound.play()
+                    mywin.flip()
+                    core.wait(negative_reinforcement_delay)
+                break # Break out of inner loop if anywhere on screen is touched
+        if not touched:
+            write_data(i + 1, 'FALSE', 'FALSE', trialClock.getTime(), 'FALSE')
+    else:
+        while globalClock.getTime() < session_timeout_time:
+            if trialClock.getTime() >= 2.0:
+                write_data(i + 1, 'TRUE', 'FALSE', trialClock.getTime(), 'FALSE')
                 click_sound.play()
                 mywin.flip()
                 core.wait(positive_reinforcement_delay)
-            else:
-                dataFile.write(f'{info["name"]},{trialClock.getTime()},type,no\n')
+                break
+            event.clearEvents()
+            dis_shape.draw()
+            mywin.update()
+            touch_tracker.clickReset()
+            if touch_tracker.getPressed(getTime=True)[0][0] == 1:
+                write_data(i + 1, 'TRUE', 'TRUE', trialClock.getTime(), 'FALSE')
                 neg_reinforce_sound.play()
                 mywin.flip()
                 core.wait(negative_reinforcement_delay)
-            break # Break out of inner loop if anywhere on screen is touched
+                break # Break out of inner loop if anywhere on screen is touched
     i += 1
-
-    
-    
-    #buttons, times = touch_tracker.
-#pause, so you get a chance to see it!
-#core.wait(5.0)
